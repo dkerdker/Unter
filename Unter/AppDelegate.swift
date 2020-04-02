e02c480287f5f924741dd66aabb9a44c099a2a43
Dee Ker <dker95@icloud.com>
2020-04-03 00:57:44 +0800

    add applicationWillResignActive code

--- Unter/AppDelegate.swift
+++ Unter/AppDelegate.swift
@@ -32,6 +32,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func applicationWillResignActive(_ application: UIApplication) {
+        //add code
+    }
 
 }
 
